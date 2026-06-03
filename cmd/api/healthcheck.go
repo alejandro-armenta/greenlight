45b53ecf0a3a4aee8b75a6fdb21c5ba013623e82
Alejandro <armentaayalaalejandro@gmail.com>
2026-06-03 17:55:48 -0600

    routing with new router

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -11,14 +11,3 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 	fmt.Fprintf(w, "version: %s\n", version)
 }
 
-func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
-}
-
-func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
-}
