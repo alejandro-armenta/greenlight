3b8664d1e4fd9f4035d668f4058e0a57b145f1b3
Alejandro <armentaayalaalejandro@gmail.com>
2026-06-04 14:01:57 -0600

    made custom function

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -9,9 +9,11 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+type envelope map[string]any
 
-	js, err := json.Marshal(data)
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+
+	js, err := json.MarshalIndent(data, "", "\t")
 
 	if err != nil {
 		return err
