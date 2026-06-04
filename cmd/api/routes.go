1a47d1e376708368e1d5ba61d4fee2d06b74cc6a
Alejandro <armentaayalaalejandro@gmail.com>
2026-06-04 16:53:28 -0600

    error handling

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -7,11 +7,17 @@ import (
 )
 
 func (app *application) routes() http.Handler {
+
 	router := httprouter.New()
+
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+
+	//este es en el router
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 
-	return router
+	return app.recoverPanic(router)
 }
