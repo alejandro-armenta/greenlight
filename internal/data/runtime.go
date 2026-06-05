8b78a39ab272224cb361f6ae427fbeb96f269994
Alejandro <armentaayalaalejandro@gmail.com>
2026-06-04 22:37:08 -0600

    made unmarshalling

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,11 +1,42 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
+
 type Runtime int
+
+// json value es un string
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	number, err := strconv.Atoi(parts[0])
+
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	//esto es porque esta facilito
+	*r = Runtime(number)
+
+	return nil
+}
 
 func (r Runtime) MarshalJSON() ([]byte, error) {
 
