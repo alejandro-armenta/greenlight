2306751a54251cfbddb6aaf7f289175ff8d11d44
Alejandro <armentaayalaalejandro@gmail.com>
2026-06-06 15:17:34 -0600

    made migrations

---
+++ migrations/000001_create_movies_table.up.sql
@@ -0,0 +1,10 @@
+create table if not exists movies(
+  id bigint primary key generated always as identity,
+  created_at timestamp(0) with time zone not null default now(),
+  title text not null,
+  year integer not null,
+  runitme integer not null,
+  genres text[] not null,
+  version integer not null default 1
+);
+
