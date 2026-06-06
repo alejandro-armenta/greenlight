77a07f9c2ac33a120263444e01fc68323370a339
Alejandro <armentaayalaalejandro@gmail.com>
2026-06-06 15:56:27 -0600

    made migrations

--- migrations/000001_create_movies_table.up.sql
+++ migrations/000001_create_movies_table.up.sql
@@ -3,7 +3,7 @@ create table if not exists movies(
   created_at timestamp(0) with time zone not null default now(),
   title text not null,
   year integer not null,
-  runitme integer not null,
+  runtime integer not null,
   genres text[] not null,
   version integer not null default 1
 );
