93e4223626a17c3df965c82fc24b89e49a0ba0a0
Alejandro <armentaayalaalejandro@gmail.com>
2026-06-08 16:12:12 -0600

    made validation

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -6,10 +6,12 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 
 	"github.com/julienschmidt/httprouter"
+	"greenlight.alexarmenta.net/internal/validator"
 )
 
 type envelope map[string]any
@@ -109,4 +111,50 @@ func (app *application) readIDParam(r *http.Request) (int, error) {
 	}
 
 	return id, nil
+}
+
+func (app *application) readString(qs url.Values, key string, defaultValue string) string {
+
+	s := qs.Get(key)
+
+	if s == "" {
+		return defaultValue
+	}
+
+	return s
+
+}
+
+func (app *application) readCSV(qs url.Values, key string, defaultValue []string) []string {
+
+	//string a,b,c
+	csv := qs.Get(key)
+
+	if csv == "" {
+		//array [a,b,c]
+		return defaultValue
+	}
+
+	//array [a,b,c]
+	ale := strings.Split(csv, ",")
+
+	return ale
+}
+
+func (app *application) readInt(qs url.Values, key string, defaultValue int, v *validator.Validator) int {
+
+	s := qs.Get(key)
+
+	if s == "" {
+		return defaultValue
+	}
+
+	i, err := strconv.Atoi(s)
+
+	if err != nil {
+		v.AddError(key, "must be an integer value")
+		return defaultValue
+	}
+
+	return i
 }
