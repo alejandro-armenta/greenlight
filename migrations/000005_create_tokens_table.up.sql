d0810b1182a8f5917ba02c56bec367acde53f250
Alejandro <armentaayalaalejandro@gmail.com>
2026-06-12 16:05:11 -0600

    made activation

---
+++ migrations/000005_create_tokens_table.up.sql
@@ -0,0 +1,14 @@
+create table if not exists tokens(
+    
+    --sha256 hash of token is stored in tokens
+    hash bytea primary key,
+    --hace que se borren las entradas de aqui si se borra el user de alla
+    --on delete restrict
+    user_id bigint not null references users on delete cascade,
+
+    --this will be 3 days 
+    expiry timestamp(0) with time zone not null,
+
+    --activation or authentication
+    scope text not null
+);
