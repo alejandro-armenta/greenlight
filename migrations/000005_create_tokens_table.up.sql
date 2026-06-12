f5cdc3daa5ff844566db4d8571a71c2f6599d354
Alejandro <armentaayalaalejandro@gmail.com>
2026-06-12 16:14:54 -0600

    made drop table

--- migrations/000005_create_tokens_table.up.sql
+++ migrations/000005_create_tokens_table.up.sql
@@ -2,6 +2,7 @@ create table if not exists tokens(
     
     --sha256 hash of token is stored in tokens
     hash bytea primary key,
+    
     --hace que se borren las entradas de aqui si se borra el user de alla
     --on delete restrict
     user_id bigint not null references users on delete cascade,
